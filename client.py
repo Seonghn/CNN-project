bad0c334002a7ed558bd40074d79cd131315fd7f
lky9620 <61020702+lky9620@users.noreply.github.com>
2020-05-26 19:30:07 +0900

    Add files via upload

---
+++ client.py
@@ -0,0 +1,24 @@
+import cv2
+import socket
+import numpy as np
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('220.149.236.91', 8080))
+
+cam = cv2.VideoCapture(0)
+ 
+cam.set(3, 320);
+cam.set(4, 240);
+ 
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+ 
+while True:
+    ret, frame = cam.read()
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+    data = numpy.array(frame)
+    stringData = data.tostring()
+
+    #(str(len(stringData))).encode().ljust(16)
+    s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
+ 
+cam.release()
---
+++ server.py
@@ -0,0 +1,32 @@
+import socket
+import cv2
+import numpy as np
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+ 
+HOST='220.149.236.91'
+PORT=8080
+ 
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+conn,addr=s.accept()
+ 
+while True:
+    length = recvall(conn, 16)
+    stringData = recvall(conn, int(length))
+    data = np.fromstring(stringData, dtype = 'uint8')
+    
+    frame = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    cv2.imshow('ImageWindow',frame)
+    cv2.waitKey(1)
