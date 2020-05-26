1444455e5f7ed0ec84e175c6d99c12621cc11f5d
lky9620 <61020702+lky9620@users.noreply.github.com>
2020-05-26 18:43:21 +0900

    Update servo.py

--- servo.py
+++ servo.py
@@ -4,24 +4,20 @@ import time
 pin =18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
-p= GPIO.PWM(pin, 50)  #PMW:ÆÞ½º Æø º¯Á¶
+p= GPIO.PWM(pin, 50)
 p.start(0)
 cnt = 0
 
 try:
 
     while True:
-        p.ChangeDutyCycle(12.5) #ÃÖ´ñ°ª
-        time.sleep(1)
         p.ChangeDutyCycle(10.0)
         time.sleep(1)
-        p.ChangeDutyCycle(7.5) #0
+        p.ChangeDutyCycle(7.5)
         time.sleep(1)
         p.ChangeDutyCycle(5.0)
         time.sleep(1)
-        p.ChangeDutyCycle(2.5) #ÃÖ¼Ú°ª
-        time.sleep(1)
-
+        
 except KeybordInterrupt:
      p.stop()
     
