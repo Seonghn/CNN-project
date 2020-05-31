600c3e910655fb138f90a79e169db89d7699b307
lky9620 <61020702+lky9620@users.noreply.github.com>
2020-05-31 18:40:09 +0900

    차선인식투

---
+++ exam.py
@@ -0,0 +1,38 @@
+import cv2
+import sys
+import math
+import numpy as np
+
+cap = cv2.VideoCapture("C:/Users/password - 1234/Downloads/video1.mp4")
+
+while (cap.isOpened()):
+
+    ret, src = cap.read()
+    src = cv2.resize(src, (640, 360))
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    can = cv2.Canny(gray, 50, 200, None, 3)
+
+    height = can.shape[0]
+    rectangle = np.array([[(0, height), (0, 140), (640, 140), (640, height)]])
+    mask = np.zeros_like(can)
+    cv2.fillPoly(mask, rectangle, 255)
+    masked_image = cv2.bitwise_and(can, mask)
+
+    ccan = cv2.cvtColor(masked_image, cv2.COLOR_GRAY2BGR)
+
+    linesP = cv2.HoughLinesP(masked_image, 1, np.pi / 180, 50, None, 50, 30)
+
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv2.line(ccan, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
+
+    mimg = cv2.addWeighted(src, 1.0, ccan, 1.0, 0.0)
+    cv2.imshow("original", src)
+    cv2.imshow('Detect Lines',mimg)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
