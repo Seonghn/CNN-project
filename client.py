573c802829771f9ad911e29324388dc58490dd07
lky9620 <61020702+lky9620@users.noreply.github.com>
2020-06-08 12:34:42 +0900

    Update client.py

--- client.py
+++ client.py
@@ -7,8 +7,8 @@ s.connect(('220.149.236.91', 8080))
 
 cam = cv2.VideoCapture(0)
  
-cam.set(3, 320);
-cam.set(4, 240);
+cam.set(3, 640);
+cam.set(4, 360);
  
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
  
@@ -20,5 +20,7 @@ while True:
 
     #(str(len(stringData))).encode().ljust(16)
     s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
+    a = s.recv(1024)
+    print(a)
  
 cam.release()
